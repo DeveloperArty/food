2a77d447c19012455408259a1dfeece5aa15ee52
artypablo <arpavlov@av.ru>
2018-03-03 14:45:24 +0300

    Initial Commit

---
+++ TopProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TopProject
+//
+//  Created by Artem Pavlov on 03.03.2018.
+//  Copyright © 2018 Artem Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
